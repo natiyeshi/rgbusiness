0c26a68976bf6017a96131c943785904c4b12ae8
Natnael Yeshiwas <natiyeshi16@gmail.com>
2024-10-01 17:51:38 +0300

    T

--- src/components/custom/TestimoniesWraper.tsx
+++ src/components/custom/TestimoniesWraper.tsx
@@ -9,18 +9,18 @@ const TestimonialWraper = async () => {
 
 async function getTestimonials(): Promise<ITestimonial[]> {
   try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/testimonial`,
-      {
-        cache: "no-store",
-      }
-    );
+    const res = await fetch(`https://rg-business.vercel.app/api/testimonial`, {
+      cache: "no-store",
+    });
     if (!res.ok) {
       throw new Error("Failed to fetch");
     }
     const testimonials: ITestimonial[] = await res.json();
+    console.log(testimonials, "Testimonials Data");
+
     return testimonials;
   } catch (error) {
+    console.log(error,"Testimonials error")
     return [];
   }
 }
