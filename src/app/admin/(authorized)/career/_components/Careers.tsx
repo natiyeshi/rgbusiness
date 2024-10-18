145df9e58965b3c27cf62d78eeed83e005296aea
Natnael Yeshiwas <natiyeshi16@gmail.com>
2024-10-18 16:27:53 +0300

    233

--- src/app/admin/(authorized)/career/_components/Careers.tsx
+++ src/app/admin/(authorized)/career/_components/Careers.tsx
@@ -17,7 +17,7 @@ const Careers = ({ initialCareers }: Props) => {
   const handleDelete = async (id: string) => {
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/career/${id}`,
+        `/api/career/${id}`,
         {
           method: "DELETE",
         }
