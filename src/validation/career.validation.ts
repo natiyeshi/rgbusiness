1c1ccbc27ea4e0d1070c807d5c7c5a746da8939a
Natnael Yeshiwas <natiyeshi16@gmail.com>
2024-10-18 16:37:38 +0300

    23322

--- src/validation/career.validation.ts
+++ src/validation/career.validation.ts
@@ -4,13 +4,13 @@ export const careerValidation = Yup.object().shape({
   title: Yup.string()
     .required("Title is required")
     .min(3, "Title must be at least 3 characters")
-    .max(300, "Title must be less than 300 characters"),
+    .max(100, "Title must be less than 100 characters"),
   qualifications: Yup.array()
     .of(
       Yup.string()
         .required("Qualification is required")
         .min(2, "Qualification must be at least 2 characters")
-        .max(100, "Qualification must be less than 100 characters")
+        .max(300, "Qualification must be less than 300 characters")
     )
     .required("Qualifications are required")
     .min(1, "At least one qualification is required"),
